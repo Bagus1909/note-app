22900d7b2fe86b34b264c4b89f191bf4d6cfb0ce
Bagus1909 <bagussetiawan19092000@gmail.com>
2024-09-07 00:56:39 +0700

    debuging index.js

--- backend/index.js
+++ backend/index.js
@@ -86,41 +86,42 @@ app.post("/create-account", async (req, res) => {
 
 // Login
 app.post("/login", async (req, res) => {
-  const { email, password } = req.body
+  res.json({ message: "Login endpoint reached" })
+  // const { email, password } = req.body
 
-  if (!email) {
-    return res.status(400).json({ message: "Email is required" })
-  }
+  // if (!email) {
+  //   return res.status(400).json({ message: "Email is required" })
+  // }
 
-  if (!password) {
-    return res.status(400).json({ message: "Password is required" })
-  }
+  // if (!password) {
+  //   return res.status(400).json({ message: "Password is required" })
+  // }
 
-  const userInfo = await User.findOne({ email: email })
+  // const userInfo = await User.findOne({ email: email })
 
-  if (!userInfo) {
-    return res.status(400).json({ message: "User not found" })
-  }
+  // if (!userInfo) {
+  //   return res.status(400).json({ message: "User not found" })
+  // }
 
-  if (userInfo.email == email && userInfo.password == password) {
-    const user = { user: userInfo }
+  // if (userInfo.email == email && userInfo.password == password) {
+  //   const user = { user: userInfo }
 
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-      expiresIn: "30m",
-    })
+  //   const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+  //     expiresIn: "30m",
+  //   })
 
-    return res.json({
-      error: false,
-      message: "Login Succesful",
-      email,
-      accessToken,
-    })
-  } else {
-    return res.status(400).json({
-      error: true,
-      message: "Invalid Credentials",
-    })
-  }
+  //   return res.json({
+  //     error: false,
+  //     message: "Login Succesful",
+  //     email,
+  //     accessToken,
+  //   })
+  // } else {
+  //   return res.status(400).json({
+  //     error: true,
+  //     message: "Invalid Credentials",
+  //   })
+  // }
 })
 
 // Get User
